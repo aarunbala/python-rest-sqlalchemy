e59d394bfd18c3b28a89258dc1103d3ce7fc8267
aarunbala <aarunbala@gmail.com>
2020-08-18 22:08:08 +1000

    Updating app

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'bingotango'
 api = Api(app)
 
+db.init_app(app)
 @app.before_first_request
 def create_tables():
     db.create_all()
@@ -27,5 +28,5 @@ api.add_resource(UserRegister, '/register')
 api.add_resource(Store, '/store/<string:name>')
 api.add_resource(StoreList, '/stores')
 
-db.init_app(app)
-app.run(port=5000, debug=True)
+if __name__ == '__main__':
+    app.run(port=5000, debug=True)
